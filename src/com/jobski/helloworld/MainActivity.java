f691353e684b805f4344ed3c952662ae96ae3bf5
Jobo Kapunan <jobo@jobski>
2013-08-18 17:35:28 +1000

    first working compile

--- src/com/jobski/helloworld/MainActivity.java
+++ src/com/jobski/helloworld/MainActivity.java
@@ -3,9 +3,12 @@ package com.jobski.helloworld;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
-
+    public final static String EXTRA_MESSAGE = "com.jobski.helloworld.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +22,14 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+	//Display mesage activity
+	Intent intent = new Intent(this, DisplayMessageActivity.class);
+	EditText editText = (EditText) findViewById(R.id.edit_message);
+	String message = editText.getText().toString();
+	intent.putExtra(EXTRA_MESSAGE, message);
+	startActivity(intent);
+    }
 }
